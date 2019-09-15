2cc280be683a7dc4be7a46c3065dbac340423ed5
Frank <yudangneu@gmail.com>
2019-09-15 20:37:18 +1200

    add codes.

--- UnpackingSequence.py
+++ UnpackingSequence.py
@@ -6,5 +6,14 @@ x, y = p
 print('x = ', x)
 print('y = ', y)
 
+data = [ 'ACME', 50, 91.1, (2012, 12, 21)]
+name, share, price, date = data
+print('name :', name)
+print('data :', date)
 
+name, shares, price, (year, mon, day) = data
+print('name :', name)
+print('year :', year)
 
+p = (4, 5)
+x, y, z = p
